5a861d62b24494cf920e3e451e805cf8588376dd
softabhi <99878096+softabhi@users.noreply.github.com>
2023-03-03 10:24:19 +0530

    added all fronted folder

---
+++ src/services/helper.js
@@ -0,0 +1,3 @@
+// export const BASE_URL = "http://localhost:6010"
+export const BASE_URL = "https://harsmernappbacked.onrender.com"
+// export const BASE_URL = "https://mernappbackend.onrender.com"
